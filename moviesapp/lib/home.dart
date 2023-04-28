8999cb4f535f15be8fcd5e55a848f406ea5446a7
Ibrahim Badr <hamedb.ibrahim@gmail.com>
2023-04-28 15:52:39 +0300

    Grid & List views features added

--- moviesapp/lib/home.dart
+++ moviesapp/lib/home.dart
@@ -14,6 +14,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String _selectedValue = categories[0];
+  bool _displayGrid = true;
+
   void _navigateToMovie(Results response) {
     Navigator.push(
         context,
@@ -41,20 +43,39 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             children: [
               // Drop down menu
-              DropdownButton(
-                  value: _selectedValue,
-                  items: categories
-                      .map((e) => DropdownMenuItem(
-                            child: Text(e),
-                            value: e,
-                          ))
-                      .toList(),
-                  onChanged: (value) {
-                    APIService.api.fetchMoviesInfo(value!);
-                    setState(() {
-                      _selectedValue = value;
-                    });
-                  }),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  DropdownButton(
+                      value: _selectedValue,
+                      items: categories
+                          .map((e) => DropdownMenuItem(
+                                child: Text(e),
+                                value: e,
+                              ))
+                          .toList(),
+                      onChanged: (value) {
+                        APIService.api.fetchMoviesInfo(value!);
+                        setState(() {
+                          _selectedValue = value;
+                        });
+                      }),
+                  IconButton(
+                    icon: const Icon(Icons.grid_view),
+                    onPressed: () {
+                      _displayGrid = true;
+                      setState(() {});
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.list),
+                    onPressed: () {
+                      _displayGrid = false;
+                      setState(() {});
+                    },
+                  ),
+                ],
+              ),
               // Separator Sized Box.
               const SizedBox(
                 height: 7,
@@ -65,30 +86,82 @@ class _MyHomePageState extends State<MyHomePage> {
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
                     MovieResponse movieResponse = snapshot.data!;
-                    return ListView.separated(
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemCount: movieResponse.results?.length ?? 0,
-                      itemBuilder: (context, index) => ListTile(
-                        onTap: () {
-                          _navigateToMovie(movieResponse.results![index]);
-                        },
-                        leading: Image.network(
-                          "${imageURL}${movieResponse.results![index].posterPath}",
-                        ),
-                        title: Text(
-                          "${movieResponse.results![index].title} (${movieResponse.results![index].releaseDate?.split("-")[0]})",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16.0,
-                          ),
-                        ),
-                        subtitle: Text(
-                            "Rate: ${movieResponse.results![index].voteAverage} ⭐"),
-                      ),
-                      separatorBuilder: (BuildContext context, int index) =>
-                          Divider(),
-                    );
+                    return _displayGrid
+                        ? GridView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: movieResponse.results?.length ?? 0,
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              crossAxisSpacing: 8,
+                              mainAxisSpacing: 8,
+                            ),
+                            itemBuilder: (context, index) => GestureDetector(
+                              onTap: () {
+                                _navigateToMovie(movieResponse.results![index]);
+                              },
+                              child: Card(
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  children: [
+                                    Expanded(
+                                      child: Image.network(
+                                        "${imageURL}${movieResponse.results![index].posterPath}",
+                                        fit: BoxFit.contain,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            "${movieResponse.results![index].title} (${movieResponse.results![index].releaseDate?.split("-")[0]})",
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16.0,
+                                            ),
+                                          ),
+                                          const SizedBox(height: 4),
+                                          Text(
+                                            "Rate: ${movieResponse.results![index].voteAverage} ⭐",
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          )
+                        : ListView.separated(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: movieResponse.results?.length ?? 0,
+                            itemBuilder: (context, index) => ListTile(
+                              onTap: () {
+                                _navigateToMovie(movieResponse.results![index]);
+                              },
+                              leading: Image.network(
+                                "${imageURL}${movieResponse.results![index].posterPath}",
+                              ),
+                              title: Text(
+                                "${movieResponse.results![index].title} (${movieResponse.results![index].releaseDate?.split("-")[0]})",
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                              subtitle: Text(
+                                  "Rate: ${movieResponse.results![index].voteAverage} ⭐"),
+                            ),
+                            separatorBuilder:
+                                (BuildContext context, int index) =>
+                                    const Divider(),
+                          );
                   } else if (snapshot.hasError) {
                     return Center(
                       child: Text(snapshot.error.toString()),
